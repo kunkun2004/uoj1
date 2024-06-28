a4dbc89ac4e062fdf8614af9c29b3aecac9fe1f4
killerwhaleskadi <112710549+killerwhaleskadi@users.noreply.github.com>
2024-06-28 15:36:27 +0800

    a

--- web/app/controllers/uidsubmit.php
+++ web/app/controllers/uidsubmit.php
@@ -271,7 +271,7 @@ $('#contest-countdown').countdown(<?= $contest['end_time']->getTimestamp() - UOJ
 
 
 
-	<div class="tab-pane" id="tab-submit-answer">
+	<div class="tab-pane active" id="tab-submit-answer">
 		<div class="top-buffer-sm"></div>
 		<h5 class="row col-sm-12">
 	C语言请选择"C"，C++请选择"C++11"，java请选择"java11"，python请选择"python3"。交错语言可以重新提交，使用其他语言的提交将不计成绩。
